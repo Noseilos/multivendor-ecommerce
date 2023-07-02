1c0849848eade3933b8935a7e270b6a4920e84c7
Noseilos <jonaseguiron@gmail.com>
2023-07-02 18:10:51 +0800

    admin backend: added admin store function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -38,4 +39,46 @@ class AdminController extends Controller
         return view('admin.admin_login');
 
     }// END AdminLogin
+
+
+
+
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+
+        return view('admin.admin_profile', compact('adminData'));
+
+    }// END AdminProfile
+
+
+
+
+
+    public function AdminProfileStore(Request $request){
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->username = $request->username;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address; 
+
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        return redirect()->back();
+
+    } // END AdminProfileStore 
 }
