36426ba75015a0327b54d1e4fcd8ffb8df052779
Noseilos <jonaseguiron@gmail.com>
2023-07-02 14:14:30 +0800

    added admin index

--- routes/web.php
+++ routes/web.php
@@ -32,10 +32,16 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+// ----------- Admin Dashboard ----------- //
+
 Route::middleware(['auth', 'role:admin'])->group(function() {
 
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
 });
+
+
+
+// ----------- Vendor Dashboard ----------- //
 
 Route::middleware(['auth', 'role:vendor'])->group(function() {
 
