83dad67dc9b60ec004bd887e96b179e54bae5b02
Noseilos <jonaseguiron@gmail.com>
2023-07-02 13:12:30 +0800

    added role middleware

--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,12 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
+Route::middleware(['auth', 'role:admin'])->group(function() {
+
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+});
+
+Route::middleware(['auth', 'role:vendor'])->group(function() {
+
+    Route::get('/vendor/dashboard', [VendorController::class, 'VendorDashboard'])->name('vendor.dashboard');
+});
