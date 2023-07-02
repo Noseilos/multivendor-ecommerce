0b8dda49f1b6efcf059feeadabb2a351616a11bf
Noseilos <jonaseguiron@gmail.com>
2023-07-02 23:08:19 +0800

    vendor backend: added vendor dashboard

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -8,7 +8,7 @@ class VendorController extends Controller
 {
     public function VendorDashboard(){
 
-        return view('vendor.vendor_dashboard');
+        return view('vendor.index');
 
     }// END VendorDashboard
 }
