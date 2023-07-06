6bafd03e9ee175dfb48423c1b1a3b0d47d846a40
Noseilos <jonaseguiron@gmail.com>
2023-07-06 21:58:35 +0800

    user frontend: user change password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -65,4 +65,29 @@ class UserController extends Controller
 
         return redirect('/login');
     }// END UserLogout
+
+
+
+
+
+    public function UserUpdatePassword(Request $request){
+        // Validation 
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed', 
+        ]);
+
+        // Match The Old Password
+        if (!Hash::check($request->old_password, auth::user()->password)) {
+            return back()->with("error", "Old Password Doesn't Match!!");
+        }
+
+        // Update The new password 
+        User::whereId(auth()->user()->id)->update([
+            'password' => Hash::make($request->new_password)
+
+        ]);
+        return back()->with("status", " Password Changed Successfully");
+
+    }// END UserLogout
 }
--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/dashboard', [UserController::class, 'UserDashboard'])->name('user.dashboard');
     Route::post('/user/profile/store', [UserController::class, 'UserProfileStore'])->name('user.profile.store');
     Route::get('/user/logout', [UserController::class, 'UserLogout'])->name('user.logout');
+    Route::post('/user/update/password', [UserController::class, 'UserUpdatePassword'])->name('user.update.password');
 
 });
 
