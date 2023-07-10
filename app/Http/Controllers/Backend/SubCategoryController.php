20605c95ab8dcb259e2393bc17327ad261172ac3
Noseilos <jonaseguiron@gmail.com>
2023-07-11 00:41:42 +0800

    admin backend: subcategory all added

---
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\SubCategory;
+
+class SubCategoryController extends Controller
+{
+    public function AllSubCategory(){
+
+        $subcategories = SubCategory::latest()->get();
+        return view('backend.subcategory.subcategory_all',compact('subcategories'));
+
+    } // END AllSubCategory 
+
+
+
+
+
+    
+}
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\VendorController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\SubCategoryController;
 
 
 /*
@@ -105,5 +106,19 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
         Route::post('/update/category' , 'UpdateCategory')->name('update.category');
         Route::get('/delete/category/{id}' , 'DeleteCategory')->name('delete.category');
 
+    });
+
+
+
+    Route::controller(SubCategoryController::class)->group(function(){
+
+        // ----------- SubCategory Routes ----------- //
+        Route::get('/all/subcategory' , 'AllSubCategory')->name('all.subcategory');
+        Route::get('/add/category' , 'AddCategory')->name('add.category');
+        Route::post('/store/category' , 'StoreCategory')->name('store.category');
+        Route::get('/edit/category/{id}' , 'EditCategory')->name('edit.category');
+        Route::post('/update/category' , 'UpdateCategory')->name('update.category');
+        Route::get('/delete/category/{id}' , 'DeleteCategory')->name('delete.category');
+    
     });
 });
