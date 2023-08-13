2af67e0fe8f919c7029161f57b4211fba6a097fe
Noseilos <jonaseguiron@gmail.com>
2023-08-13 20:04:11 +0800

    backend: vendor add product blade added

--- app/Http/Controllers/Backend/VendorProductController.php
+++ app/Http/Controllers/Backend/VendorProductController.php
@@ -22,4 +22,21 @@ class VendorProductController extends Controller
         $products = Product::where('vendor_id',$id)->latest()->get();
         return view('vendor.backend.product.vendor_product_all',compact('products'));
     } // End Method 
+
+    public function VendorAddProduct(){
+
+        $brands = Brand::latest()->get();
+        $categories = Category::latest()->get();
+        return view('vendor.backend.product.vendor_product_add',compact('brands','categories'));
+
+    } // End Method 
+
+
+    public function VendorGetSubCategory($category_id){
+
+        $subcat = SubCategory::where('category_id',$category_id)->orderBy('subcategory_name','ASC')->get();
+        
+        return json_encode($subcat);
+
+    }// End Method 
 }
