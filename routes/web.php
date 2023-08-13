dc87f6fd7e895ada93b6e2382532d89443f46a6b
Noseilos <jonaseguiron@gmail.com>
2023-08-13 21:34:37 +0800

    backend: add banner

--- routes/web.php
+++ routes/web.php
@@ -191,11 +191,11 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
         Route::get('/delete/slider/{id}' , 'DeleteSlider')->name('delete.slider');
     
     });
-    
+
     Route::controller(BannerController::class)->group(function(){
         Route::get('/all/banner' , 'AllBanner')->name('all.banner');
-        Route::get('/add/slider' , 'AddSlider')->name('add.slider');
-        Route::post('/store/slider' , 'StoreSlider')->name('store.slider');
+        Route::get('/add/banner' , 'AddBanner')->name('add.banner');
+        Route::post('/store/banner' , 'StoreBanner')->name('store.banner');
         Route::get('/edit/slider/{id}' , 'EditSlider')->name('edit.slider');
         Route::post('/update/slider' , 'UpdateSlider')->name('update.slider');
         Route::get('/delete/slider/{id}' , 'DeleteSlider')->name('delete.slider');
