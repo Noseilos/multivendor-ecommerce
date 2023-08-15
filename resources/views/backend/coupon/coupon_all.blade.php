5319355a55c5862a0696da3281d23bb0b0a88611
Noseilos <jonaseguiron@gmail.com>
2023-08-16 07:39:46 +0800

    backend: edit coupon system added

--- resources/views/backend/coupon/coupon_all.blade.php
+++ resources/views/backend/coupon/coupon_all.blade.php
@@ -15,7 +15,7 @@
             </div>
             <div class="ms-auto">
                 <div class="btn-group">
-                    <a href="{{ route('add.subcategory') }}" class="btn btn-primary">Add Coupon</a>
+                    <a href="{{ route('add.coupon') }}" class="btn btn-primary">Add Coupon</a> 	
                 </div>
             </div>
         </div>
@@ -41,7 +41,7 @@
                                 <tr>
                                     <td> {{ $key + 1 }} </td>
                                     <td> {{ $item->coupon_name }}</td>
-                                    <td> { $item->coupon_discount }} </td>
+                                    <td> {{ $item->coupon_discount }}% </td>
                                     <td> {{ Carbon\Carbon::parse($item->coupon_validity)->format('D, d F Y') }} </td>
 
 
@@ -55,9 +55,9 @@
                                     </td>
 
                                     <td>
-                                        <a href="{{ route('edit.subcategory', $item->id) }}" class="btn btn-info">Edit</a>
-                                        <a href="{{ route('delete.subcategory', $item->id) }}" class="btn btn-danger"
-                                            id="delete">Delete</a>
+                                        
+                                        <a href="{{ route('edit.coupon',$item->id) }}" class="btn btn-info">Edit</a>
+                                        <a href="{{ route('delete.coupon',$item->id) }}" class="btn btn-danger" id="delete" >Delete</a>
 
                                     </td>
                                 </tr>
