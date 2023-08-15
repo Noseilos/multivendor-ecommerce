9d9211d2042a571922790f198a3f8997d4752dc7
Noseilos <jonaseguiron@gmail.com>
2023-08-16 07:50:35 +0800

    backend: ship division added

---
+++ app/Http/Controllers/Backend/ShippingAreaController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\ShipDivision;
+use App\Models\ShipDistricts;
+use App\Models\ShipState;
+use Carbon\Carbon;
+
+class ShippingAreaController extends Controller
+{
+    public function AllDivision(){
+        $division = ShipDivision::latest()->get();
+        return view('backend.ship.division.division_all',compact('division'));
+    } // End Method 
+
+    public function AddDivision(){
+        return view('backend.ship.division.division_add');
+    }// End Method 
+
+
+    public function StoreDivision(Request $request){ 
+
+        ShipDivision::insert([ 
+            'division_name' => $request->division_name, 
+        ]);
+
+       $notification = array(
+            'message' => 'ShipDivision Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.division')->with($notification); 
+
+    }// End Method 
+
+    public function EditDivision($id){
+
+        $division = ShipDivision::findOrFail($id);
+        return view('backend.ship.division.division_edit',compact('division'));
+
+    }// End Method 
+
+
+     public function UpdateDivision(Request $request){
+
+        $division_id = $request->id;
+
+         ShipDivision::findOrFail($division_id)->update([
+            'division_name' => $request->division_name,
+        ]);
+
+       $notification = array(
+            'message' => 'ShipDivision Updated Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.division')->with($notification); 
+
+
+    }// End Method 
+
+
+    public function DeleteDivision($id){
+
+        ShipDivision::findOrFail($id)->delete();
+
+         $notification = array(
+            'message' => 'ShipDivision Deleted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification); 
+
+
+    }// End Method 
+}
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Backend\VendorProductController;
 use App\Http\Controllers\Backend\SliderController;
 use App\Http\Controllers\Backend\BannerController;
 use App\Http\Controllers\Backend\CouponController;
+use App\Http\Controllers\Backend\ShippingAreaController;
 use App\Http\Controllers\Frontend\IndexController;
 use App\Http\Controllers\Frontend\CartController;
 use App\Http\Controllers\User\WishlistController;
@@ -215,6 +216,17 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
         Route::post('/update/coupon' , 'UpdateCoupon')->name('update.coupon');
         Route::get('/delete/coupon/{id}' , 'DeleteCoupon')->name('delete.coupon');
     
+    }); 
+
+     // Shipping Division All Route 
+    Route::controller(ShippingAreaController::class)->group(function(){
+        Route::get('/all/division' , 'AllDivision')->name('all.division');
+        Route::get('/add/division' , 'AddDivision')->name('add.division');
+        Route::post('/store/division' , 'StoreDivision')->name('store.division');
+        Route::get('/edit/division/{id}' , 'EditDivision')->name('edit.division');
+        Route::post('/update/division' , 'UpdateDivision')->name('update.division');
+        Route::get('/delete/division/{id}' , 'DeleteDivision')->name('delete.division');
+
     }); 
 
 }); // Admin End Middleware 
