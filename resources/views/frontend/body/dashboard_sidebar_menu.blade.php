505f867ca387028f4d0c36768174d1ea986c41e9
Noseilos <jonaseguiron@gmail.com>
2023-08-20 14:13:29 +0800

    backend: return order view added

--- resources/views/frontend/body/dashboard_sidebar_menu.blade.php
+++ resources/views/frontend/body/dashboard_sidebar_menu.blade.php
@@ -14,6 +14,10 @@
                     href="{{ route('user.order.page') }}"><i class="fi-rs-shopping-bag mr-10"></i>Orders</a>
             </li>
             <li class="nav-item">
+                <a class="nav-link {{ $route == 'return.order.page' ? 'active' : '' }}"
+                    href="{{ route('return.order.page') }}"><i class="fi-rs-shopping-bag mr-10"></i>Return Requests</a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="#track-orders"><i class="fi-rs-shopping-cart-check mr-10"></i>Track Your
                     Order</a>
             </li>
--- routes/web.php
+++ routes/web.php
@@ -378,6 +378,7 @@ Route::middleware(['auth','role:user'])->group(function() {
         Route::get('/user/order_details/{order_id}' , 'UserOrderDetails');
         Route::get('/user/invoice_download/{order_id}' , 'UserOrderInvoice');  
         Route::post('/return/order/{order_id}' , 'ReturnOrder')->name('return.order');
+        Route::get('/return/order/page' , 'ReturnOrderPage')->name('return.order.page');
 
     }); 
 
