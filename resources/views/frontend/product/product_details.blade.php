38b07149813ac8a764a2472c2473a78977f550c1
Noseilos <jonaseguiron@gmail.com>
2023-08-24 05:26:58 +0800

    frontend: show approved review on product detail

--- resources/views/frontend/product/product_details.blade.php
+++ resources/views/frontend/product/product_details.blade.php
@@ -343,113 +343,118 @@
 
 
                                 <div class="tab-pane fade" id="Reviews">
-                                    <!--Comments-->
-                                    <div class="comments-area">
-                                        <div class="row">
-                                            <div class="col-lg-8">
-                                                <h4 class="mb-30">Customer questions & answers</h4>
-                                                <div class="comment-list">
-                                                    <div class="single-comment justify-content-between d-flex mb-30">
-                                                        <div class="user justify-content-between d-flex">
-                                                            <div class="thumb text-center">
-                                                                <img src="assets/imgs/blog/author-2.png" alt="" />
-                                                                <a href="#"
-                                                                    class="font-heading text-brand">Sienna</a>
-                                                            </div>
-                                                            <div class="desc">
-                                                                <div class="d-flex justify-content-between mb-10">
-                                                                    <div class="d-flex align-items-center">
-                                                                        <span class="font-xs text-muted">December 4, 2022
-                                                                            at 3:12 pm </span>
-                                                                    </div>
-                                                                    <div class="product-rate d-inline-block">
-                                                                        <div class="product-rating" style="width: 100%">
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                                <p class="mb-10">Lorem ipsum dolor sit amet, consectetur
-                                                                    adipisicing elit. Delectus, suscipit exercitationem
-                                                                    accusantium obcaecati quos voluptate nesciunt facilis
-                                                                    itaque modi commodi dignissimos sequi repudiandae minus
-                                                                    ab deleniti totam officia id incidunt? <a
-                                                                        href="#" class="reply">Reply</a></p>
-                                                            </div>
-                                                        </div>
-                                                    </div>
+                <!--Comments-->
+                <div class="comments-area">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <h4 class="mb-30">Customer questions & answers</h4>
+                            <div class="comment-list">
 
-                                                    <div class="single-comment justify-content-between d-flex">
-                                                        <div class="user justify-content-between d-flex">
-                                                            <div class="thumb text-center">
-                                                                <img src="assets/imgs/blog/author-4.png" alt="" />
-                                                                <a href="#"
-                                                                    class="font-heading text-brand">Gemma</a>
-                                                            </div>
-                                                            <div class="desc">
-                                                                <div class="d-flex justify-content-between mb-10">
-                                                                    <div class="d-flex align-items-center">
-                                                                        <span class="font-xs text-muted">December 4, 2022
-                                                                            at 3:12 pm </span>
-                                                                    </div>
-                                                                    <div class="product-rate d-inline-block">
-                                                                        <div class="product-rating" style="width: 80%">
-                                                                        </div>
-                                                                    </div>
-                                                                </div>
-                                                                <p class="mb-10">Lorem ipsum dolor sit amet, consectetur
-                                                                    adipisicing elit. Delectus, suscipit exercitationem
-                                                                    accusantium obcaecati quos voluptate nesciunt facilis
-                                                                    itaque modi commodi dignissimos sequi repudiandae minus
-                                                                    ab deleniti totam officia id incidunt? <a
-                                                                        href="#" class="reply">Reply</a></p>
-                                                            </div>
+
+                                @php
+                                    $reviews = App\Models\Review::where('product_id', $product->id)
+                                        ->latest()
+                                        ->limit(5)
+                                        ->get();
+                                @endphp
+
+                                @foreach ($reviews as $item)
+                                    @if ($item->status == 0)
+                                    @else
+                                        <div
+                                            class="single-comment justify-content-between d-flex mb-30">
+                                            <div class="user justify-content-between d-flex">
+                                                <div class="thumb text-center">
+                                                    <img src="{{ !empty($item->user->photo) ? url('upload/user_images/' . $item->user->photo) : url('upload/no_image.jpg') }}"
+                                                        alt="" />
+                                                    <a href="#"
+                                                        class="font-heading text-brand">{{ $item->user->name }}</a>
+                                                </div>
+                                                <div class="desc">
+                                                    <div class="d-flex justify-content-between mb-10">
+                                                        <div class="d-flex align-items-center">
+                                                            <span class="font-xs text-muted">
+                                                                {{ Carbon\Carbon::parse($item->created_at)->diffForHumans() }}
+                                                            </span>
+                                                        </div>
+
+                                                        <div class="product-rate d-inline-block">
+
+                                                            @if ($item->rating == null)
+                                                            @elseif($item->rating == 1)
+                                                                <div class="product-rating"
+                                                                    style="width: 20%"></div>
+                                                            @elseif($item->rating == 2)
+                                                                <div class="product-rating"
+                                                                    style="width: 40%"></div>
+                                                            @elseif($item->rating == 3)
+                                                                <div class="product-rating"
+                                                                    style="width: 60%"></div>
+                                                            @elseif($item->rating == 4)
+                                                                <div class="product-rating"
+                                                                    style="width: 80%"></div>
+                                                            @elseif($item->rating == 5)
+                                                                <div class="product-rating"
+                                                                    style="width: 100%"></div>
+                                                            @endif
                                                         </div>
                                                     </div>
+                                                    <p class="mb-10">{{ $item->comment }} <a
+                                                            href="#" class="reply">Reply</a>
+                                                    </p>
                                                 </div>
-                                            </div>
-                                            <div class="col-lg-4">
-                                                <h4 class="mb-30">Customer reviews</h4>
-                                                <div class="d-flex mb-30">
-                                                    <div class="product-rate d-inline-block mr-15">
-                                                        <div class="product-rating" style="width: 90%"></div>
-                                                    </div>
-                                                    <h6>4.8 out of 5</h6>
-                                                </div>
-                                                <div class="progress">
-                                                    <span>5 star</span>
-                                                    <div class="progress-bar" role="progressbar" style="width: 50%"
-                                                        aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%
-                                                    </div>
-                                                </div>
-                                                <div class="progress">
-                                                    <span>4 star</span>
-                                                    <div class="progress-bar" role="progressbar" style="width: 25%"
-                                                        aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%
-                                                    </div>
-                                                </div>
-                                                <div class="progress">
-                                                    <span>3 star</span>
-                                                    <div class="progress-bar" role="progressbar" style="width: 45%"
-                                                        aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">45%
-                                                    </div>
-                                                </div>
-                                                <div class="progress">
-                                                    <span>2 star</span>
-                                                    <div class="progress-bar" role="progressbar" style="width: 65%"
-                                                        aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">65%
-                                                    </div>
-                                                </div>
-                                                <div class="progress mb-30">
-                                                    <span>1 star</span>
-                                                    <div class="progress-bar" role="progressbar" style="width: 85%"
-                                                        aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%
-                                                    </div>
-                                                </div>
-                                                <a href="#" class="font-xs text-muted">How are ratings
-                                                    calculated?</a>
                                             </div>
                                         </div>
-                                    </div>
-                                    <!--comment form-->
+                                    @endif
+                                @endforeach
+
+
+                            </div>
+                        </div>
+                        <div class="col-lg-4">
+                            <h4 class="mb-30">Customer reviews</h4>
+                            <div class="d-flex mb-30">
+                                <div class="product-rate d-inline-block mr-15">
+                                    <div class="product-rating" style="width: 90%"></div>
+                                </div>
+                                <h6>4.8 out of 5</h6>
+                            </div>
+                            <div class="progress">
+                                <span>5 star</span>
+                                <div class="progress-bar" role="progressbar" style="width: 50%"
+                                    aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%
+                                </div>
+                            </div>
+                            <div class="progress">
+                                <span>4 star</span>
+                                <div class="progress-bar" role="progressbar" style="width: 25%"
+                                    aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%
+                                </div>
+                            </div>
+                            <div class="progress">
+                                <span>3 star</span>
+                                <div class="progress-bar" role="progressbar" style="width: 45%"
+                                    aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">45%
+                                </div>
+                            </div>
+                            <div class="progress">
+                                <span>2 star</span>
+                                <div class="progress-bar" role="progressbar" style="width: 65%"
+                                    aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">65%
+                                </div>
+                            </div>
+                            <div class="progress mb-30">
+                                <span>1 star</span>
+                                <div class="progress-bar" role="progressbar" style="width: 85%"
+                                    aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">85%
+                                </div>
+                            </div>
+                            <a href="#" class="font-xs text-muted">How are ratings
+                                calculated?</a>
+                        </div>
+                    </div>
+                </div>
+                <!--comment form-->
                                     <div class="comment-form">
                                         <h4 class="mb-15">Add a review</h4>
 
@@ -459,16 +464,20 @@
                                         @else
                                             <div class="row">
                                                 <div class="col-lg-8 col-md-12">
-                                                    <form class="form-contact comment_form" action="{{ route('store.review') }}" method="post" id="commentForm">
+                                                    <form class="form-contact comment_form"
+                                                        action="{{ route('store.review') }}" method="post"
+                                                        id="commentForm">
                                                         @csrf
                                                         <div class="row">
 
-                                                            <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                                            <input type="hidden" name="product_id"
+                                                                value="{{ $product->id }}">
 
-                                                            @if($product->vendor_id == NULL)
+                                                            @if ($product->vendor_id == null)
                                                                 <input type="hidden" name="hvendor_id" value="">
                                                             @else
-                                                                <input type="hidden" name="hvendor_id" value="{{ $product->vendor_id }}">
+                                                                <input type="hidden" name="hvendor_id"
+                                                                    value="{{ $product->vendor_id }}">
                                                             @endif
 
                                                             <table class="table" style=" width: 60%;">
