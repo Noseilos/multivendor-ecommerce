a8011667941d917950a691b0046468c743636949
Noseilos <jonaseguiron@gmail.com>
2023-08-24 04:41:52 +0800

    frontend: blog post categories

--- app/Http/Controllers/Backend/BlogController.php
+++ app/Http/Controllers/Backend/BlogController.php
@@ -225,4 +225,13 @@ class BlogController extends Controller
         return view('frontend.blog.blog_details',compact('blogcategoryies','blogdetails','breadcat'));
 
     }// End Method 
+
+    public function BlogPostCategory($id,$slug){
+
+        $blogcategoryies = BlogCategory::latest()->get();
+        $blogpost = BlogPost::where('category_id',$id)->get();
+        $breadcat = BlogCategory::where('id',$id)->get();
+        return view('frontend.blog.category_post',compact('blogcategoryies','blogpost','breadcat'));
+
+    }// End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -391,6 +391,7 @@ Route::controller(CartController::class)->group(function(){
 Route::controller(BlogController::class)->group(function(){
     Route::get('/blog' , 'AllBlog')->name('home.blog');  
     Route::get('/post/details/{id}/{slug}' , 'BlogDetails'); 
+    Route::get('/post/category/{id}/{slug}' , 'BlogPostCategory');  
 
 });
 
