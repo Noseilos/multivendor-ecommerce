0e2a205d18c473ecff563d619fabcdc3f07ed98b
Noseilos <jonaseguiron@gmail.com>
2023-08-26 17:40:40 +0800

    backend: add permission

--- app/Http/Controllers/Backend/RoleController.php
+++ app/Http/Controllers/Backend/RoleController.php
@@ -15,4 +15,25 @@ class RoleController extends Controller
         return view('backend.pages.permission.all_permission',compact('permissions'));
 
     } // End Method 
+
+    public function AddPermission(){
+        return view('backend.pages.permission.add_permission');
+    }// End Method 
+
+    public function StorePermission(Request $request){
+
+        $role = Permission::create([
+            'name' => $request->name,
+            'group_name' => $request->group_name,
+
+        ]);
+
+        $notification = array(
+            'message' => 'Permission Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.permission')->with($notification); 
+
+    }// End Method 
 }
--- routes/web.php
+++ routes/web.php
@@ -364,7 +364,8 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
     // Role & Permission All Route 
     Route::controller(RoleController::class)->group(function(){
         Route::get('/all/permission' , 'AllPermission')->name('all.permission');
-    
+        Route::get('/add/permission' , 'AddPermission')->name('add.permission');
+        Route::post('/store/permission' , 'StorePermission')->name('store.permission');
     });
 
 }); // Admin End Middleware 
