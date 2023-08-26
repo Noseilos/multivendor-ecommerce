d4693d8eac8911aaaa9d1d324e6594a702604758
Noseilos <jonaseguiron@gmail.com>
2023-08-26 16:56:49 +0800

    backend: installed spatie laravel permissions

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Intervention\Image\ImageServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,Spatie\Permission\PermissionServiceProvider::class,
         /*
          * Application Service Providers...
          */
