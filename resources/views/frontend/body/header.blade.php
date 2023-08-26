b2f75fba33b49aad935bd1763e66c497fccc896f
Noseilos <jonaseguiron@gmail.com>
2023-08-26 14:02:26 +0800

    frontend: search for product

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -74,7 +74,9 @@
                 </div>
                 <div class="header-right">
                     <div class="search-style-2">
-                        <form action="#">
+                        <form action="{{ route('product.search') }}" method="post">
+                            @csrf
+
                             <select class="select-active">
                                 <option>All Categories</option>
                                 <option>Milks and Dairies</option>
@@ -88,7 +90,7 @@
                                 <option>Noodles & Rice</option>
                                 <option>Ice cream</option>
                             </select>
-                            <input type="text" placeholder="Search for items..." />
+                            <input name="search" placeholder="Search for items..." />
                         </form>
                     </div>
                     <div class="header-action-right">
--- routes/web.php
+++ routes/web.php
@@ -429,6 +429,11 @@ Route::controller(ReviewController::class)->group(function(){
    
 });
 
+// Search All Route 
+Route::controller(IndexController::class)->group(function(){
+    Route::post('/search' , 'ProductSearch')->name('product.search'); 
+});
+
 /// User All Route
 Route::middleware(['auth','role:user'])->group(function() {
 
