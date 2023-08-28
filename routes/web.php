94302b9c101a3944688506c5a7c31f84db1b1d44
Noseilos <jonaseguiron@gmail.com>
2023-08-28 08:32:09 +0800

    backend: edit and update admin

--- routes/web.php
+++ routes/web.php
@@ -394,6 +394,8 @@ Route::middleware(['auth', 'role:admin'])->group(function(){
         Route::get('/all/admin' , 'AllAdmin')->name('all.admin');
         Route::get('/add/admin' , 'AddAdmin')->name('add.admin');
         Route::post('/admin/user/store' , 'AdminUserStore')->name('admin.user.store');
+        Route::get('/edit/admin/role/{id}' , 'EditAdminRole')->name('edit.admin.role');
+        Route::post('/admin/user/update/{id}' , 'AdminUserUpdate')->name('admin.user.update');
    
     });
 
